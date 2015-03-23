f547522b94b0f8ed252d34636049a8a80750fb34
lukas.srom <lukas.srom@gmail.com>
2015-03-23 20:28:31 +0100

    DtbManager implementation
    Scripts for creating MOVIES and PERSONS tables in dtb implemented in
    class DtbManager.

--- Task 3/MovieManager/src/main/java/com/mycompany/moviemanager/DtbManager.java
+++ Task 3/MovieManager/src/main/java/com/mycompany/moviemanager/DtbManager.java
@@ -3,6 +3,11 @@
  */
 package com.mycompany.moviemanager;
 
+import static com.mycompany.moviemanager.PersonManagerImpl.log;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author Lukáš Šrom
@@ -10,28 +15,59 @@ package com.mycompany.moviemanager;
  * @date 2015 4 3
  */
 public class DtbManager {
+    private static final String LOGIN = "administrator";
+    private static final String PASSWORD = "admin";
+    private static final String URL = "jdbc:derby://localhost:1527/MovieManagerDtb;";
+    private static final String DRIVER = "org.apache.derby.jdbc.ClientDriver";
     
-    /**
-     * Searches database for object given as parameter.
-     * @param obj is object for which is database searched.
-     */
-    public void searchDtb (Object obj) throws ServiceFailureException {
-        throw new UnsupportedOperationException ("Not supported yet.");
+    public static void main(String[] args){
+        System.out.println("this is main");
+        
+        try{
+            createTablePersons();
+        }catch(ClassNotFoundException ex){}
+        try{
+            createTableMovies();
+        }catch(ClassNotFoundException ex){}
     }
     
-    /**
-     * Modifies object in database.
-     * @param obj is data of object to be modified.
-     */
-    public void modifyDtb (Object obj) throws ServiceFailureException{
-        throw new UnsupportedOperationException ("Not supported yet.");
+    public static void createTablePersons() throws ClassNotFoundException{
+        // to connect to dtb
+        Class.forName(DRIVER);
+        
+        // try to connect to dtb, if not possible or when it's done, session will be automatically terminated
+        try(Connection conn = DriverManager.getConnection(URL, LOGIN, PASSWORD);){
+            conn.prepareStatement("CREATE TABLE PERSONS (\n" +
+                    "id INTEGER NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,\n" +
+                    "name VARCHAR(128) NOT NULL,\n" +
+                    "birthday VARCHAR(128),\n" + 
+                    "movies VARCHAR(128)\n" +
+                    ")").executeUpdate();
+        }catch (SQLException ex){
+            log.error("Database connection problems!", ex);
+            throw new ServiceFailureException("Error when creating table PERSONS!", ex);
+        };
     }
     
-    /**
-     * Adds object to database.
-     * @param obj is object to be added to database.
-     */
-    public void addToDtb (Object obj) throws ServiceFailureException{
-        throw new UnsupportedOperationException ("Not supported yet.");
+    public static void createTableMovies() throws ClassNotFoundException{
+        // to connect to dtb
+        Class.forName(DRIVER);
+        
+        // try to connect to dtb, if not possible or when it's done, session will be automatically terminated
+        try(Connection conn = DriverManager.getConnection(URL, LOGIN, PASSWORD);){
+            conn.prepareStatement("CREATE TABLE PERSONS (\n" +
+                    "id INTEGER NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,\n" +
+                    "title VARCHAR(128) NOT NULL,\n" +
+                    "movieYear VARCHAR(128),\n" + 
+                    "genre VARCHAR(128),\n" +
+                    "length INTEGER,\n" +
+                    "director VARCHAR(128),\n" +
+                    "writer VARCHAR(128),\n" +
+                    "movieCast VARCHAR(256)\n" +
+                    ")").executeUpdate();
+        }catch (SQLException ex){
+            log.error("Database connection problems!", ex);
+            throw new ServiceFailureException("Error when creating table PERSONS!", ex);
+        };
     }
 }
